ce3844555f1baeed2f179517806169efa8447ad9
NickyPeto <petovellon@gmail.com>
2023-05-05 10:41:06 +0200

    Added env files to gitignore and compressed assets

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,28 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject, catchError, map, of } from 'rxjs';
+import { Subject, map } from 'rxjs';
+import { environment } from 'src/env/environment';
 
 @Injectable({
   providedIn: 'any',
 })
 export class WeatherService {
+  //TODO: type this
   cityData = new Subject<any>();
-  url: string = '';
+  private url: string = '';
   private query: string = 'stockholm';
-  //This should be normally set in env variables but I'll do it like this rn to save time :)
-  private apiKey: string = '63252697c8994fc3b7b115338230405';
 
   constructor(private http: HttpClient) {}
 
   public getWeatherData(city: string) {
     this.query = JSON.stringify(city.toLowerCase());
-    //This should be normally set in env variables but I'll do it like this rn to save time :)
-    this.url = `http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.query}&aqi=no`;
+
+    this.url = environment.apiUrl + `&q=${this.query}&aqi=no`;
     this.http
       .get(this.url)
       .pipe(map((items: Object) => new Array(items)))
-      .subscribe((value: any[]) => {
-        return this.cityData.next(value);
-      });
+      .subscribe((value: any[]) => this.cityData.next(value));
   }
 }
